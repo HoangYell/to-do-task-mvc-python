644d12d3c6a800fdd271e5bfb3745bc7dda1e570
yellcamap <ngohoang.yell@gmail.com>
2018-08-26 13:41:49 +0700

    status 404 error, add cancel button, home button

--- controller.py
+++ controller.py
@@ -21,24 +21,20 @@ def index(method, get, post, headers):
     works = work_manager.all()
     status = '200 OK'
     view = View("templates\index.html")
-    body = view.render(works=works,
-                       current_time=get_current_local_iso_time(),
-                       limit_time=get_limit_local_iso_time())
+    body = view.render(works=works)
 
     if method == 'POST':
         status = '303 See Other'
         body = b''
-
         headers.append(('Location', '/'))
-        print("=============================>")
-        print(post)
-        print("<=============================")
+
         work_name = get_first_element(post, 'work_name', '').strip()
         starting_date = get_first_element(post, 'starting_date', '')
         ending_date = get_first_element(post, 'ending_date', '')
         work_status = get_first_element(post, 'work_status', '').strip()
         id = get_first_element(post, 'id')
         is_delete = get_first_element(post, 'is_delete')
+
         work = Work(work_name, starting_date, ending_date, work_status, id=id)
         if is_delete:
             work_manager.delete(work)
@@ -51,13 +47,16 @@ def edit(method, get, post, headers):
     works = work_manager.all()
     status = '200 OK'
     view = View("templates\edit.html")
-    body = None
+    body = b''
 
     if method == 'GET':
         headers.append(('Location', '/'))
         id = get_first_element(get, 'id', '')
-        work = [w for w in works if w.id == id][0]
-        body = view.render(id=id, work=work)
+        works_by_id = [w for w in works if w.id == id]
+        if len(works_by_id)>0:
+            body = view.render(id=id, work=works_by_id[0])
+        else:
+            status = '404 Not Found'
     return status, body
 
 
@@ -65,13 +64,16 @@ def delete(method, get, post, headers):
     works = work_manager.all()
     status = '200 OK'
     view = View("templates\delete.html")
-    body = None
+    body = b''
 
     if method == 'GET':
         headers.append(('Location', '/'))
         id = get_first_element(get, 'id', '')
-        work = [w for w in works if w.id == id][0]
-        body = view.render(id=id, work=work)
+        works_by_id = [w for w in works if w.id == id]
+        if len(works_by_id)>0:
+            body = view.render(id=id, work=works_by_id[0])
+        else:
+            status = '404 Not Found'
     return status, body
 
 
@@ -83,6 +85,5 @@ def calendar(method, get, post, headers):
                        "start": work.starting_date,
                        "end": work.ending_date} for work in works]
     body = view.render(calendar_works=json.dumps(calendar_works),
-                       current_time=get_current_local_iso_time(),
-                       limit_time=get_limit_local_iso_time())
+                       current_time=get_current_local_iso_time())
     return status, body
